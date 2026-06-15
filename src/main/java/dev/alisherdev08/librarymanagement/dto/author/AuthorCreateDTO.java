d2f6de671a8efc0efe1832e3b8c09fd587801052
alisherdev08 <githubalisherka@gmail.com>
2026-06-15 06:16:18 -0400

    Add fields to Author create, update, and response DTOs

--- src/main/java/dev/alisherdev08/librarymanagement/dto/author/AuthorCreateDTO.java
+++ src/main/java/dev/alisherdev08/librarymanagement/dto/author/AuthorCreateDTO.java
@@ -1,4 +1,15 @@
 package dev.alisherdev08.librarymanagement.dto.author;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
 public class AuthorCreateDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate birthDate;
 }
--- src/main/java/dev/alisherdev08/librarymanagement/dto/author/AuthorResponseDTO.java
+++ src/main/java/dev/alisherdev08/librarymanagement/dto/author/AuthorResponseDTO.java
@@ -1,4 +1,16 @@
 package dev.alisherdev08.librarymanagement.dto.author;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
 public class AuthorResponseDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate birthDate;
 }
