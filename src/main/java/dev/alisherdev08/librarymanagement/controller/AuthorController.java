0140108e0f27940d13b51be76a96f2de175c532b
alisherdev08 <githubalisherka@gmail.com>
2026-06-16 06:08:33 -0400

    Implement CRUD operations in Author module

--- src/main/java/dev/alisherdev08/librarymanagement/controller/AuthorController.java
+++ src/main/java/dev/alisherdev08/librarymanagement/controller/AuthorController.java
@@ -1,4 +1,36 @@
 package dev.alisherdev08.librarymanagement.controller;
 
+import dev.alisherdev08.librarymanagement.entity.Author;
+import dev.alisherdev08.librarymanagement.service.AuthorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/authors")
+@RequiredArgsConstructor
 public class AuthorController {
+    private final AuthorService authorService;
+
+    @PostMapping
+    public Author createAuthor(@RequestBody Author author){
+         return  authorService.createAuthor(author);
+    }
+    @GetMapping("/{id}")
+    public Author getAuthorById(@PathVariable Long id){
+        return authorService.findAuthorById(id);
+    }
+    @GetMapping
+    public List<Author> getAllAuthors(){
+        return authorService.findAllAuthors();
+    }
+    @PutMapping("/{id}")
+    public Author updateAuthor(@PathVariable Long id, @RequestBody Author author){
+        return authorService.updateAuthor(id, author);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteAuthor(@PathVariable Long id){
+        authorService.deleteAuthor(id);
+    }
 }
--- src/main/java/dev/alisherdev08/librarymanagement/service/AuthorService.java
+++ src/main/java/dev/alisherdev08/librarymanagement/service/AuthorService.java
@@ -1,4 +1,35 @@
 package dev.alisherdev08.librarymanagement.service;
 
+import dev.alisherdev08.librarymanagement.entity.Author;
+import dev.alisherdev08.librarymanagement.repository.AuthorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class AuthorService {
+    private final AuthorRepository authorRepository;
+
+    public Author createAuthor(Author author){
+       return authorRepository.save(author);
+    }
+    public Author findAuthorById(Long id){
+        return authorRepository.findById(id).orElse(null);
+    }
+    public List<Author> findAllAuthors(){
+        return authorRepository.findAll();
+    }
+    public Author updateAuthor(Long id, Author request){
+        Author author = authorRepository.findById(id).orElseThrow(() -> new RuntimeException("Author not found."));
+        author.setFirstName(request.getFirstName());
+        author.setLastName(request.getLastName());
+        author.setEmail(request.getEmail());
+        author.setBirthDate(request.getBirthDate());
+        return authorRepository.save(author);
+    }
+    public void deleteAuthor(Long id){
+        authorRepository.deleteById(id);
+    }
 }
